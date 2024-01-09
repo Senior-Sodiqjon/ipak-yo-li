d70633f8a2f81fb1fe1fbbf2f1c25903acec38d5
sodiqovsodiqjon <115532144+sodiqovsodiqjon@users.noreply.github.com>
2024-01-09 18:42:38 +0500

    modent telecom cardlar o'zgardi

--- public/frontend/js/script-min.js
+++ public/frontend/js/script-min.js
@@ -12103,5 +12103,19 @@ $p.on("click", function (e) {
   new Ant();
 
 
+   function openModal() {
+      document.getElementById('loginModal').classList.add('show-modal');
+    }
+
+    function closeModal() {
+      document.getElementById('loginModal').classList.remove('show-modal');
+    }
+
+    // Modalni yopish uchun, overlay bo'lib o'tgan joyga bosing
+    window.onclick = function (event) {
+      if (event.target === document.getElementById('loginModal')) {
+        closeModal();
+      }
+    }
   
-  
+    
